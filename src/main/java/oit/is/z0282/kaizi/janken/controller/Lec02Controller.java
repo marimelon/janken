db7b94295602f41474fa1ccd7ce2132c70fe6d81
marimelon <29140293+marimelon@users.noreply.github.com>
2020-10-19 21:11:25 +0900

    ログイン中の他のユーザを表示する

--- src/main/java/oit/is/z0282/kaizi/janken/controller/Lec02Controller.java
+++ src/main/java/oit/is/z0282/kaizi/janken/controller/Lec02Controller.java
@@ -1,6 +1,9 @@
 package oit.is.z0282.kaizi.janken.controller;
 
+import java.security.Principal;
+
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,18 +13,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import oit.is.z0282.kaizi.janken.model.Janken;
+import oit.is.z0282.kaizi.janken.model.Entry;
 
 @Controller
 @RequestMapping("/lec02")
 public class Lec02Controller {
+
+  @Autowired
+  private Entry entry;
+
   @GetMapping
-  public String get(@RequestParam(required = false) Optional<String> hand, ModelMap model) {
+  public String get(@RequestParam(required = false) Optional<String> hand,Principal prin, ModelMap model) {
     hand.ifPresent(val -> {
       Janken janken = new Janken(val);
       model.addAttribute("phand", val);
       model.addAttribute("chand", janken.getCpuHand());
       model.addAttribute("result", janken.getResult());
     });
+
+    String loginUser = prin.getName();
+    this.entry.addUser(loginUser);
+    model.addAttribute("entry", this.entry);
+
     return "lec02.html";
   }
 
